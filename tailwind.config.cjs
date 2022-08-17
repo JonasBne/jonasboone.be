267a8b3100cf2acc460052776ce9efc454ecff75
Jonas <boone.jonas@outlook.be>
2022-08-17 20:21:58 +0200

    feat: tailwind setup + styled header and navigation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        colors: {
+            blue: {
+                primary: "#111827",
+                secondary: "#2b4865",
+            },
+            green: {
+                primary: "#8fe3cf",
+                secondary: "#007357",
+            },
+            grey: {
+                primary: "#aaaaaa",
+            },
+            white: {
+                primary: "#ffffff",
+            },
+        },
+        fontFamily: {
+            primary: ["Roboto", "sans-serif"],
+            secondary: ["Montserrat", "sans-serif"],
+        },
+    },
+    plugins: [],
+};
